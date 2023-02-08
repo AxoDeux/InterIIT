1194598c247e86517db2c2cda80daa3cce7b6920
Ishan Sawant <2019meb1265@iitrpr.ac.in>
2023-02-08 22:26:52 +0530

    obstacle detection of enemies

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
-
+using Pathfinding;
+[RequireComponent(typeof(AIPath))]
+[RequireComponent(typeof(AIDestinationSetter))]
 public class EnemyFollow : MonoBehaviour
 {
     public float speed;
@@ -14,17 +16,29 @@ public class EnemyFollow : MonoBehaviour
     private float angle;    //to point at player
 
     Animator anim;
+
+    private AIPath aiPath;
+    private AIDestinationSetter destinationSetter;
+
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        aiPath = GetComponent<AIPath>();
+        destinationSetter = GetComponent<AIDestinationSetter>();
+    }
+
+    private void Start() {
+        destinationSetter.target = target;
+        float rndNum = Random.Range(1.5f, 3f);
+        aiPath.maxSpeed = rndNum;
     }
 
     void Update()
     {
-        if (!GameManager.isRewinding)
-            FollowPlayer();
+        if(!GameManager.isRewinding) { }
+            //FollowPlayer();
     }
 
     private void FollowPlayer()
