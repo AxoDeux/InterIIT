4962a044286fbd2df3750f18128037522220fb18
Ishan Sawant <2019meb1265@iitrpr.ac.in>
2023-02-08 16:44:17 +0530

    a star import

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyFollow : MonoBehaviour
 {
@@ -12,34 +13,33 @@ public class EnemyFollow : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 lookDir;
     private float angle;    //to point at player
-
     Animator anim;
-    private void Awake()
-    {
-        anim = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
+    //private void Awake()
+    //{
+    //    anim = GetComponentInChildren<Animator>();
+    //    rb = GetComponent<Rigidbody2D>();
+    //    target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    //}
 
-    void Update()
-    {
-        if (!GameManager.isRewinding)
-            FollowPlayer();
-    }
+    //void Update()
+    //{
+    //    if (!GameManager.isRewinding)
+    //        FollowPlayer();
+    //}
 
-    private void FollowPlayer()
-    {
-        if (Vector2.Distance(target.position, transform.position) > stoppingDistance)
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        if (anim != null)
-            anim.SetTrigger("isWalking");
-        //RotateEnemy();
-    }
+    //private void FollowPlayer()
+    //{
+    //    if (Vector2.Distance(target.position, transform.position) > stoppingDistance)
+    //        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    //    if (anim != null)
+    //        anim.SetTrigger("isWalking");
+    //    //RotateEnemy();
+    //}
 
-    private void Rotate()
-    {
-        lookDir = target.position - transform.position;
-        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
-    }
+    //private void Rotate()
+    //{
+    //    lookDir = target.position - transform.position;
+    //    angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+    //    rb.rotation = angle;
+    //}
 }
